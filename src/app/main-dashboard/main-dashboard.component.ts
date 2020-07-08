976290507c6324bfaf4ae5b28afddb62cae31344
shehani <janadari1995@gmail.com>
2020-07-08 20:07:37 +0530

    7/8

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { AuthenticationService } from '../service/authentication.service';
+import { Router } from '@angular/router';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-main-dashboard',
@@ -15,10 +20,30 @@ export class MainDashboardComponent implements OnInit {
   showSearchApplication = false;
   showProgressBar = false;
 
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  // dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  // @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+ 
+
+  // shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+  constructor(private _formBuilder: FormBuilder,private dialog: MatDialog,private authService: AuthenticationService,private router: Router)  { }
+
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  events: string[] = [];
+  opened: boolean;
+
   shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
-  constructor(private _formBuilder: FormBuilder,private dialog: MatDialog,)  { }
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
@@ -26,10 +51,10 @@ export class MainDashboardComponent implements OnInit {
       secondCtrl: ['', Validators.required]
     });
   }
+  
 goforPrediction(){
   
     this.dialog.open(DashboardComponent, {
-      
     width: '80%',
     height: 'auto',
     maxHeight: '90vh',
@@ -57,5 +82,31 @@ console.log("search");
     
 
   }
-  
+  logOut(){
+   
+      return this.authService.logout().then(() => {
+        this.router.navigate(['/']);
+      })
+    
+  }
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
